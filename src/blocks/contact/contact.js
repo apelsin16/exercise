eb10d2f3e94894e35135f61f3b4cb68d92dad5e5
Apelsin16 <apelsin@i.ua>
2019-04-19 17:07:00 +0300

    ready

---
+++ src/blocks/contact/contact.js
@@ -0,0 +1,30 @@
+
+    // // var platform = new H.service.Platform({
+    // // 'app_id': 'fOiha8xBedlZsIiHWZ2U',
+    // // 'app_code': 'RxWErTfy4EXjDMyhAM61VQ'
+    // // });
+
+    // // // Obtain the default map types from the platform object
+    // // var maptypes = platform.createDefaultLayers();
+
+    // // // Instantiate (and display) a map object:
+    // // var map = new H.Map(
+    // // document.getElementById('mapContainer'),
+    // // maptypes.normal.map,
+    // // {
+    // //   zoom: 10,
+    // //   center: { lng: 30.4, lat: 52.51 }
+    // // });
+
+
+    // function initMap() {
+    //     // The location of Uluru
+    //     var uluru = {lat: -25.344, lng: 131.036};
+    //     // The map, centered at Uluru
+    //     var map = new google.maps.Map(
+    //         document.getElementById('mapContainer'), {zoom: 4, center: uluru});
+    //     // The marker, positioned at Uluru
+    //     var marker = new google.maps.Marker({position: uluru, map: map});
+    //   }
+        
+          
--- src/js/import/blocks.js
+++ src/js/import/blocks.js
@@ -1,3 +1,4 @@
 import "%blocks%/header/header.js";
 import "%blocks%/footer/footer.js";
+import "%blocks%/contact/contact.js";
 import "%blocks%/main/main.js";
